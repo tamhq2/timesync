d3e9855667be64d32893cba7748dedc8796ec1bf
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2024-03-31 12:15:22 +0700

    Update timesync2024.cmd

--- timesync2024.cmd
+++ timesync2024.cmd
@@ -1,71 +1,17 @@
-@setlocal DisableDelayedExpansion
-@echo off
-
-set "_cmdf=%~f0"
-for %%# in (%*) do (
-if /i "%%#"=="r1" set r1=1
-if /i "%%#"=="r2" set r2=1
+set _Args=%*
+if “%~1” NEQ “” (
+set _Args=%_Args:”=%
 )
-
-if exist %SystemRoot%\Sysnative\cmd.exe if not defined r1 (
-setlocal EnableDelayedExpansion
-start %SystemRoot%\Sysnative\cmd.exe /c ""!_cmdf!" %* r1"
-exit /b
+fltmc 1>nul 2>nul || (
+cd /d “%~dp0”
+cmd /u /c echo Set UAC = CreateObject^(“Shell.Application”^) : UAC.ShellExecute “cmd.exe”, “/k cd “”%~dp0″” && “”%~dpnx0″” “”%_Args%”””, “”, “runas”, 1 > “%temp%\mitvietnam.vbs”
+“%temp%\mitvietnam.vbs”
+del /f /q “%temp%\mitvietnam.vbs” 1>nul 2>nul
+exit
 )
-
-:: Re-launch the script with ARM32 process if it was initiated by x64 process on ARM64 Windows
-
-if exist %SystemRoot%\SysArm32\cmd.exe if %PROCESSOR_ARCHITECTURE%==AMD64 if not defined r2 (
-setlocal EnableDelayedExpansion
-start %SystemRoot%\SysArm32\cmd.exe /c ""!_cmdf!" %* r2"
-exit /b
-)
-
-::  Set Path variable, it helps if it is misconfigured in the system
-
-set "PATH=%SystemRoot%\System32;%SystemRoot%\System32\wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
-if exist "%SystemRoot%\Sysnative\reg.exe" (
-set "PATH=%SystemRoot%\Sysnative;%SystemRoot%\Sysnative\wbem;%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\;%PATH%"
-)
-
-::  Check LF line ending
-
-pushd "%~dp0"
->nul findstr /rxc:".*" "%~nx0"
-if not %errorlevel%==0 (
-echo:
-echo Error: Script either has LF line ending issue, or it failed to read itself.
-echo:
-popd
-ping 127.0.0.1 -n 6 > nul
-exit /b
-)
-popd
-
-::========================================================================================================================================
-@echo off
-cls
-set _args=
-set _elev=
-set _args=%*
-if defined _args set _args=%_args:"=%
-if defined _args (
-for %%A in (%_args%) do (
-if /i "%%A"=="-el"                    set _elev=1
-)
-)
-
-::========================================================================================================================================
-
-::  Elevate script as admin and pass arguments and preventing loop
-
->nul fltmc || (
-if not defined _elev %nul% %psc% "start cmd.exe -arg '/c \"!_PSarg:'=''!\"' -verb runas" && exit /b
-%nceline%
-echo This script require administrator privileges.
-echo To do so, right click on this script and select 'Run as administrator'.
-goto MASend
-)
+@shift /0
+CLS
+@COLOR b
 net stop w32time & w32tm /unregister && w32tm /register && powershell -exe bypass Restart-Service w32Time && w32tm /resync
 echo.
 echo.
