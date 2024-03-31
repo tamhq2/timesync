afabfe1c9307b6684143c84fea1f13805a606aab
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2024-03-31 12:25:28 +0700

    Update timesync2024.cmd

--- timesync2024.cmd
+++ timesync2024.cmd
@@ -1,3 +1,17 @@
+set _Args=%*
+if “%~1” NEQ “” (
+set _Args=%_Args:”=%
+)
+fltmc 1>nul 2>nul || (
+cd /d “%~dp0”
+cmd /u /c echo Set UAC = CreateObject^(“Shell.Application”^) : UAC.ShellExecute “cmd.exe”, “/k cd “”%~dp0″” && “”%~dpnx0″” “”%_Args%”””, “”, “runas”, 1 > “%temp%\mitvietnam.vbs”
+“%temp%\mitvietnam.vbs”
+del /f /q “%temp%\mitvietnam.vbs” 1>nul 2>nul
+exit
+)
+@shift /0
+CLS
+@COLOR b
 @echo off
 cls
 net stop w32time & w32tm /unregister && w32tm /register && powershell -exe bypass Restart-Service w32Time && w32tm /resync
